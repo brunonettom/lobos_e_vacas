907c14d3e3c8834301240ed04a9c025e5404a0d1
brunonm <brunonm@al.insper.edu.br>
2024-09-19 17:40:04 -0300

    tava sem commit, to mandando agora

--- cow_wolf.py
+++ cow_wolf.py
@@ -10,11 +10,54 @@ class DangerDetector(MobileNetDetector):  # Agora herdamos de MobileNetDetector
         # Ajuste o valor de confiança para o valor que você achar melhor
         self.CONFIDENCE = CONFIDENCE
 
-    def separar_caixa_entre_animais(self, img: np.ndarray, resultados: list) -> (np.ndarray, dict):
-        
+    def separar_caixa_entre_animais(self, img: np.ndarray, resultados: list): #-> (np.ndarray, dict):
+        # img (np.ndarray): Imagem de saida - as detecções são desenhadas apenas se "self.draw = True"
+        # resultados ( list(dict) ): Lista de dicionários com as detecções (classe, confidence, bbox(x1, y1, x2, y2))
+        #{'classe': 'cow', 'confidence': 0.9906375, 'bbox': (379, 131, 181, 120)},
         """Não mude ou renomeie esta função."""
         img = img.copy()
-        animais = {'vaca': [], 'lobo': []}
+        animais = {'vaca': [], 'lobo': [],'n_lobos':0, 'n_vacas':0}
+        n_lobos=0
+        n_vacas=0
+        xys=[]
+
+        for elemento in resultados:
+
+            if elemento["classe"]=="cow":
+                n_vacas+=1
+                
+                x_esquerda_vaca, y_esquerda_vaca, x_direita_vaca, y_direita_vaca = elemento["bbox"]
+
+                animais["vaca"].append(elemento)
+            else:
+                x_esquerda_lobo_atual, y_esquerda_lobo_atual, x_direita_lobo_atual, y_direita_lobo_atual = elemento["bbox"]
+                if n_lobos == 0:
+                    x_esquerda_lobo, y_esquerda_lobo, x_direita_lobo, y_direita_lobo = x_esquerda_lobo_atual, y_esquerda_lobo_atual, x_direita_lobo_atual, y_direita_lobo_atual
+                else:
+                    x_esquerda_lobo = min(x_esquerda_lobo, x_esquerda_lobo_atual)
+                    y_esquerda_lobo = min(y_esquerda_lobo, y_esquerda_lobo_atual)
+                    x_direita_lobo = max(x_direita_lobo, x_direita_lobo_atual)
+                    y_direita_lobo = max(y_direita_lobo, y_direita_lobo_atual)
+                n_lobos+=1
+                animais["lobo"].append(elemento)
+        animais['n_lobos']=n_lobos
+        animais['n_vacas']=n_vacas
+        if n_vacas>0:
+            img = cv2.rectangle(img, (x_esquerda_vaca, y_esquerda_vaca), (x_direita_vaca, y_direita_vaca), (0, 255, 0), 2)
+        if n_lobos>0: 
+            img =cv2.rectangle(img, (x_esquerda_lobo, y_esquerda_lobo), (x_direita_lobo, y_direita_lobo), (255, 0,0 ), 2)
+            xys = (x_esquerda_lobo, y_esquerda_lobo, x_direita_lobo, y_direita_lobo)
+            if n_vacas > 0 and n_lobos > 0:
+                if x_esquerda_lobo < x_esquerda_vaca < x_direita_lobo and x_esquerda_lobo < x_direita_vaca < x_direita_lobo and y_esquerda_lobo < y_esquerda_vaca < y_direita_lobo and y_esquerda_lobo < y_direita_vaca < y_direita_lobo:
+                    animais['perigo'] = True
+                else:
+                    animais['perigo'] = False
+        else:
+            animais['perigo']=False
+        
+            
+
+
         return img, animais
 
     def calcula_iou(self, boxA: list, boxB: list) -> float:
@@ -38,14 +81,14 @@ class DangerDetector(MobileNetDetector):  # Agora herdamos de MobileNetDetector
 
 def main():
     import time
-    bgr = cv2.imread("img/cow_wolf05.png")
+    bgr = cv2.imread("img/cow_wolf02.png")
 
     Detector = DangerDetector()
     image, results = Detector.detect(bgr)
-    image, animais = Detector.separar_caixa_entre_animais(image, results)
-    print(animais)
-    image = Detector.checar_perigo(image, animais)
+    print (results)
 
+    image, animais = Detector.separar_caixa_entre_animais(image, results)
+    image = Detector.checar_perigo(image, animais)
     cv2.imshow("Result_MobileNet", image)
     cv2.waitKey(0)
 
--- module_aruco.py
+++ module_aruco.py
@@ -27,28 +27,39 @@ class Aruco3d():
                 self.camera_distortion = np.loadtxt(camera_distortion, delimiter=',')
 
     def detectaAruco(self,bgr):
-        # Gera a mascara em escalas de Cinza apartir da copia da imagem em BGR
+        # Gera a mascara em escalas de Cinza apartir da copia da imagem em BGR #preto e branco
         gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
         #Define o Dicionario para o Aruco que vamos utilizar
-        dicionarioAruco = aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
-        
+        dicionarioAruco = aruco.getPredefinedDictionary(aruco.DICT_6X6_1000) #NECESSÁRIO PRA ACHAR AS COORDENADAS E O ID DO ARUCO
         #Detecta os Arucos e Carrega os pontos de translação e de rotação do aruco em relação ao robo
-        cornersList, ids, _ = aruco.detectMarkers(gray, dicionarioAruco)
-        
+        #ACHA ARUCO E ID
+        cornersList, ids, _ = aruco.detectMarkers(gray, dicionarioAruco) #rastreia
         results = []
         # Se um Id foi detectado, verifica se ele esta dentro da range de 0 a 99 e calcula os valores de rotação e translação 
         if ids is not None:
             for i in range(len(ids)):
+                # ACHA VE
                 ret = aruco.estimatePoseSingleMarkers(cornersList[i], 6, self.camera_matrix, self.camera_distortion)
+                    # Lista de cantos
+                        #cornersList = [...]  
+                    # Matriz de calibração da câmera
+                        # camera_matrix = [...]  
+                    # camera_distortion = [...]  # Coeficientes de distorção da câmera
+                    # ret = aruco.estimatePoseSingleMarkers(cornersList[i], 6, camera_matrix, camera_distortion)
+
+                    # # Extraindo a translação e rotação
+                    # translation_vector = ret[0]
+                    # rotation_vector = ret[1]
                 rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
                     
                 results.append({
                     'id': ids[i],
                     'rvec': rvec,
                     'tvec': tvec,
-                    'distancia':np.linalg.norm(tvec),
+                    'distancia':np.linalg.norm(tvec), #DISTÂNCIA DA COORDENADA - HIPOTENUSA
                     'corners': cornersList[i],
                     'centro': np.mean(cornersList[i], axis=1).astype("int").flatten()
+                        #axis=1 --> média de eixo a eixo dos 4 pontos
                 })
         
         #retorna os ids e as coordenadas de centro e de distancia do aruco em relação ao robo
@@ -57,12 +68,16 @@ class Aruco3d():
     def drawAruco(self, bgr, result):
             # Desenha a linha referencia em X
             cv2.line(bgr, (bgr.shape[1]//2,bgr.shape[0]//2), ((bgr.shape[1]//2 + 50),(bgr.shape[0]//2)), (0,0,255), 5) 
+                #CONVERTER HSV: bgr_color = cv2.cvtColor(hsv_color, cv2.COLOR_HSV2BGR)
             # Desenha a linha referencia em Y
             cv2.line(bgr, (bgr.shape[1]//2,bgr.shape[0]//2), (bgr.shape[1]//2,(bgr.shape[0]//2 + 50)), (0,255,0), 5) 
 
             #-- Desenha um retanculo e exibe Id do marker encontrado
+            # DESENHA OS EIXOS DO AROCO
             cv2.drawFrameAxes(bgr, self.camera_matrix, self.camera_distortion, result['rvec'], result['tvec'] ,0.03)
-
+                #parametros da câmera: camera_matrix, camera_distortion
+                #vetor rotação: result['rvec']
+                #vetor posição: result['tvec'] 
             aruco.drawDetectedMarkers(bgr, np.array([result['corners']]), np.array([result['id']]))
 
             return bgr
